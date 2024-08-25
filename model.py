d6814deef83985e0178672f8c7f31ae24e901d8a
Ellen Xu <ellen2imagine@gmail.com>
2024-08-24 22:31:21 -0700

    simplify model

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.distributions.categorical import Categorical
-from torch.distributions.multivariate_normal import MultivariateNormal
 
 def mlp(hidden_sizes, activation=nn.Tanh, output_activation=nn.Identity):
   layers = []
@@ -38,57 +37,42 @@ class MLPCategorical(nn.Module):
     return logprob
 
 class MLPGaussian(nn.Module):
-  def __init__(self, obs_dim, hidden_sizes, act_dim, activation=nn.Tanh, log_std=0.):
+  def __init__(self, obs_dim, hidden_sizes, act_dim, activation=nn.Tanh, log_std=3.):
     super(MLPGaussian, self).__init__()
     self.mlp = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
     self.log_std = torch.nn.Parameter(torch.full((act_dim,), log_std, dtype=torch.float32))
+    self.register_buffer('std', self.log_std.exp())
 
-  def forward(self, x):
-    x = torch.FloatTensor(x).unsqueeze(0)
-    mean = self.mlp(x)
-    std = torch.exp(self.log_std)
-    covariance_mat = torch.diag(std ** 2)
-    return mean, covariance_mat
+  def forward(self, x: torch.Tensor):
+    x = x.unsqueeze(0)
+    return self.mlp(x)
 
-  def get_policy(self, obs):
-    mean, covariance_mat = self.forward(obs)
-    return MultivariateNormal(mean, covariance_mat)
+  def get_action(self, obs: torch.Tensor, deterministic=False):
+    mean = self.forward(obs)
+    action = mean[0] if deterministic else torch.normal(mean, self.std)[0]
+    return action.detach().cpu().numpy()
 
-  def get_action(self, obs, deterministic=False):
-    action = self.get_policy(obs).sample()[0]
-    if deterministic: # get mean action
-      action = self.forward(obs)[0][0]
-    # if len(action) == 1: # TODO: clean
-    #   return [action.item()]
-    # else:
-    return action.detach().cpu().numpy() #.tolist()
-
-  def get_logprob(self, obs, act):
-    logprob = self.get_policy(obs).log_prob(act)
-    return logprob
+  def get_logprob(self, obs: torch.Tensor, act: torch.Tensor):
+    mean = self.forward(obs)
+    logprob = -0.5 * (((act - mean)**2) / self.std**2 + 2 * self.log_std + torch.log(torch.tensor(2*torch.pi)))
+    return logprob.sum(dim=-1)
 
 class MLPCritic(nn.Module):
   def __init__(self, obs_dim, hidden_sizes, activation=nn.Tanh):
     super(MLPCritic, self).__init__()
     self.mlp = mlp([obs_dim] + list(hidden_sizes) + [1], activation)
 
-  def forward(self, x):
+  def forward(self, x: torch.Tensor):
     return self.mlp(x)
 
 class ActorCritic(nn.Module):
-  def __init__(self, obs_dim, hidden_sizes, act_dim, activation=nn.Tanh, discrete=False):
+  def __init__(self, obs_dim, hidden_sizes, act_dim, discrete=False):
     super(ActorCritic, self).__init__()
     self.discrete = discrete
-    if discrete:
-      self.actor = MLPCategorical(obs_dim, hidden_sizes["pi"], act_dim, activation)
-    else:
-      self.actor = MLPGaussian(obs_dim, hidden_sizes["pi"], act_dim)
+    self.actor = MLPGaussian(obs_dim, hidden_sizes["pi"], act_dim)
     self.critic = MLPCritic(obs_dim, hidden_sizes["vf"])
 
-  def forward(self, x):
-    if self.discrete:
-      actor_out = self.actor(x)
-    else:
-      actor_out = self.actor.forward(x)[0]  # mean
+  def forward(self, x: torch.Tensor):
+    actor_out, _ = self.actor(x) # mean
     critic_out = self.critic(x)
     return actor_out, critic_out
